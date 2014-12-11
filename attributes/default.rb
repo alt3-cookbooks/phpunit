f3e9fe07523f3a09fde5d48d38efe6d3461edee7
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-11 23:06:14 +0100

    Aut-follows latest stable

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # PHPUnit
 default['phpunit']['package'] = 'phpunit/phpunit'
-default['phpunit']['version'] = '4.3.*'
+default['phpunit']['version'] = '4.*'
 default['phpunit']['install_path'] = "/opt/composer-libraries/phpunit";
 default['phpunit']['bin_dir'] = "/usr/local/bin"
